3e38894879a5a9b9e19777cd29dc9e31312cd681
palakpadalia <palak@sanskartechnolab.com>
2024-05-27 10:55:57 +0530

    recruitment changes

--- sanskar_hrms/hooks.py
+++ sanskar_hrms/hooks.py
@@ -252,20 +252,20 @@ fixtures = [
             ],
         ]
     ]},
-    {"dt":"Custom Field","filters":[
-        [
-            "module","in",[
-               "Sanskar HRMS"
-            ]
-        ]
-    ]},
-    {"dt":"Property Setter","filters":[
-        [
-            "module","in",[
-               "Sanskar HRMS"
-            ],
-        ]
-    ]},
+    # {"dt":"Custom Field","filters":[
+    #     [
+    #         "module","in",[
+    #            "Sanskar HRMS"
+    #         ]
+    #     ]
+    # ]},
+    # {"dt":"Property Setter","filters":[
+    #     [
+    #         "module","in",[
+    #            "Sanskar HRMS"git
+    #         ],
+    #     ]
+    # ]},
     {"dt":"Print Format","filters":[
         [
             "module","in",[
